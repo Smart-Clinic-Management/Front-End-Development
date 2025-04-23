e6a1cbbfbcde0612119a19c3fa5f766e0700c4d4
Omnia-Abdelhamed <omnia.silmy@gmail.com>
2025-04-23 12:04:24 +0200

    edit navbar

---
+++ src/app/dashboard/pages/specialization/specialization.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialization',
+  imports: [],
+  templateUrl: './specialization.component.html',
+  styleUrl: './specialization.component.css'
+})
+export class SpecializationComponent {
+
+}
