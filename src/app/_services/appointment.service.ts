ac450860b1226bcf5c1f9e0949b1eb09a8f1875d
kareem-n <kareemnasser1369@gmail.com>
2025-05-02 20:50:53 +0300

    t

--- src/app/_services/appointment.service.ts
+++ src/app/_services/appointment.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { IPatientAppointment } from '../_interfaces/IPatientAppointment';
+import { IDoctorAppointments } from '../_interfaces/IDoctorAppointments';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor() { }
+  private baseUrl = 'https://localhost:7047/api/Appointments/';
+  constructor(private http: HttpClient) { }
+
+  GetPatientAppointment(id:string){
+    return this.http.get<IPatientAppointment>(this.baseUrl+"GetPatientAppointments/"+id)
+  }
+
+  GetDoctorAppointment(id:string){
+    return this.http.get<IDoctorAppointments>(this.baseUrl+"GetDoctorAppointments/"+id)
+  }
+  
 }
