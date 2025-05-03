b8f0a26781990beb34e4b48142208bf1e30dd360
kareem-n <kareemnasser1369@gmail.com>
2025-05-03 21:40:24 +0300

    handling doctor appointment

--- src/app/_services/appointment.service.ts
+++ src/app/_services/appointment.service.ts
@@ -2,22 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IPatientAppointment } from '../_interfaces/IPatientAppointment';
 import { IDoctorAppointments } from '../_interfaces/IDoctorAppointments';
-
+import { IAppointmentRequest } from './../_interfaces/IAppointmentsRequest';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppointmentService {
+  private baseUrl = 'https://localhost:7047/api/Appointments';
+  constructor(private http: HttpClient) {}
 
-  private baseUrl = 'https://localhost:7047/api/Appointments/';
-  constructor(private http: HttpClient) { }
-
-  GetPatientAppointment(id:string){
-    return this.http.get<IPatientAppointment>(this.baseUrl+"GetPatientAppointments/"+id)
+  GetPatientAppointment(id: string) {
+    return this.http.get<IPatientAppointment>(
+      this.baseUrl + '/GetPatientAppointments/' + id
+    );
   }
 
-  GetDoctorAppointment(id:string){
-    return this.http.get<IDoctorAppointments>(this.baseUrl+"GetDoctorAppointments/"+id)
+  GetDoctorAppointment(id: string) {
+    return this.http.get<IDoctorAppointments>(
+      this.baseUrl + '/GetDoctorAppointments/' + id
+    );
   }
   
   CreateAppointment(appointmet: IAppointmentRequest) {
