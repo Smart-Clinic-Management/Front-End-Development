e34bee5cfff7c2a00f429e8d527492dd9a692e9e
kareem-n <kareemnasser1369@gmail.com>
2025-05-04 03:01:09 +0300

    add auth interceptor

--- src/app/_services/doctor.service.ts
+++ src/app/_services/doctor.service.ts
@@ -34,7 +34,9 @@ export class DoctorService {
 
   GetDoctorWithAppointments(doctorId:number){
     const current = new Date();
-    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}` ;    
+    const date = `${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}` ; 
+    console.log(date);
+       
     return this.http.get<{ data: DoctorSchedual}>(`${this.baseUrl}/schedule/?id=${doctorId}&startDate=${date}`);
   }
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { AuthInterceptor } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     AuthGuard
   ]
 };
